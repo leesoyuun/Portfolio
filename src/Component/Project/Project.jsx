4f5b8f843b9035655308100b6aa5673fecf8e169
leesoyuun <leesoyun4206@naver.com>
2024-02-21 13:59:38 +0900

    Add: 프로젝트 페이지 추가

--- src/Component/Project/Project.jsx
+++ src/Component/Project/Project.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import * as c from "../Common/CommonStyle";
 import * as p from "./ProjectStyle";
+import ProjectJson from "../../assets/json/Project.json";
+import Test from "../../assets/img/Test.png";
 
-const Header = (props) => {
+const Project = () => {
   return (
-    <p.MainImg src={props.mainImg}/>
+    <p.Center>
+      <p.Whole>
+        <p.Headername>{`PROJECT`}</p.Headername>
+        {ProjectJson.project.map((project) => (
+          <p.TotalProject>
+            <p.ProjectImg src={Test} />
+            <p.DetailProject>
+              <p.ProjectName>{project.name}</p.ProjectName>
+              <p.ProjectDescript>{project.description}</p.ProjectDescript>
+              <p.Languages>
+              {project.useLanguage?.map((language) => (
+                <p.UseLanguage>{language}</p.UseLanguage>
+              ))}
+              </p.Languages>
+            </p.DetailProject>
+          </p.TotalProject>
+        ))}
+      </p.Whole>
+    </p.Center>
   );
 };
-export default Header;
+export default Project;
--- src/Component/Project/ProjectStyle.jsx
+++ src/Component/Project/ProjectStyle.jsx
@@ -1,6 +1,74 @@
 import styled from "@emotion/styled";
+const breakpoints = [480, 768, 992, 1280];
+const media = breakpoints.map((bp) => `@media (max-width: ${bp}px)`);
 
-export const MainImg = styled.img`
-    width: calc(100vw / 3);
-    height: calc(100vw / 3);
+export const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 0px 20% 5% 20%;
+  min-height: calc(100vh - 101px);
+  width: 100%;
+  ${media[2]} {
+    padding: 0px 0px 5% 0px;
+  }
+`;
+export const Whole = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const Headername = styled.div`
+  color: #4f66e3;
+  font-weight: 700;
+  font-size: 1.3rem;
+`;
+export const TotalProject = styled.div`
+  display: flex;
+  justify-content: cneter;
+  align-items: center;
+  margin-top: 3rem;
+  ${media[1]} {
+    flex-direction: column;
+  }
+  
+`;
+export const DetailProject = styled.div`
+  margin-left: 1.5rem;
+  width: calc((100vw - 1.5rem - 300px - 40%) / 2);
+  ${media[2]} {
+    margin-left: 0rem;
+    margin-top: 1rem;
+    width: calc(100vw - 12% - 1.5rem);
+  }
+`;
+export const ProjectName = styled.div`
+  font-weight: 600;
+  margin-bottom: 1.5rem;
+  text-align: center;
+`;
+export const ProjectDescript = styled.div`
+  color: #525252;
+  font-weight: 500;
+  line-height: 1.7rem;
+  text-align: center;
+  white-space: pre-wrap;
+`;
+export const ProjectImg = styled.img`
+  width: 300px;
+  height: 250px;
+  border-radius: 16px;
+  ${media[0]} {
+    width: 100%;
+  }
+`;
+export const Languages = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  line-height: 2rem;
+  margin-top: 1.5rem;
+`;
+export const UseLanguage = styled.div`
+  font-weight: 600;
+  margin-left: 1rem;
 `;
