e5643d2880fa38db78b23d33e0e9c14978f21e9a
leesoyuun <leesoyun4206@naver.com>
2024-02-21 14:00:58 +0900

    Add: 스타일 요소들 파일 분리

--- src/Component/Introduce/Introduce.jsx
+++ src/Component/Introduce/Introduce.jsx
@@ -2,55 +2,28 @@ import React from "react";
 import * as c from "../Common/CommonStyle";
 import * as i from "./IntroduceStyle";
 import styled from "@emotion/styled";
-import Leesoyuun from "../../assets/img/Test.png";
 
 const Center = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
-  height: calc(100vh - 300px)
+  // align-items: center;
+  height: calc(100vh - 101px);
+  width: 100%;
+  margin-top: 100px;
 `;
-const Container = styled.div`
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-const LiquidShape = styled.div`
-  border: 2px solid black;
-  width: 250px;
-  height: 250px;
-  background-image: url(${Leesoyuun});
-  background-size: cover;
-  animation: liquid 8s ease-in-out infinite;
 
-  border-radius: 60% 40% 30% 70% / 60% 30% 70% 40%;
-  @keyframes liquid {
-    0% {
-      border-radius: 60% 40% 30% 70% / 60% 30% 70% 40%;
-    }
-    50% {
-      border-radius: 30% 60% 70% 40% / 50% 60% 30% 60%;
-    }
-    100% {
-      border-radius: 60% 40% 30% 70% / 60% 30% 70% 40%;
-    }
-  }
-`;
 const Introduce = () => {
   return (
-    <c.total>
-      <Center>
-        <i.Whole>
-          <i.Title>{`Front-End Developer\nLeeSoyun 👩‍💻`}</i.Title>
-          <i.SubDescription>{`안녕하세요. 끊임없이 배우고, 발전하며 사용자 경험을 증진시키기 위해 노력하는\n프론트엔드 개발자 이소윤입니다.`}</i.SubDescription>
-          <i.MoreAbout>{`More About Me`}</i.MoreAbout>
-        </i.Whole>
-        <Container>
-          <LiquidShape></LiquidShape>
-        </Container>
-      </Center>
-    </c.total>
+    <Center>
+      <i.Whole>
+        <i.Title>{`Front-End Developer\nLeeSoyun 👩‍💻`}</i.Title>
+        <i.SubDescription>{`안녕하세요. 끊임없이 배우고, 발전하며 사용자 경험을 증진시키기 위해 노력하는\n프론트엔드 개발자 이소윤입니다.`}</i.SubDescription>
+        <i.MoreAbout>{`More About Me`}</i.MoreAbout>
+      </i.Whole>
+      <i.Container>
+        <i.LiquidShape></i.LiquidShape>
+      </i.Container>
+    </Center>
   );
 };
 export default Introduce;
--- src/Component/Introduce/IntroduceStyle.jsx
+++ src/Component/Introduce/IntroduceStyle.jsx
@@ -1,31 +1,73 @@
 import styled from "@emotion/styled";
+const breakpoints = [480, 768, 992, 1280];
+const media = breakpoints.map((bp) => `@media (max-width: ${bp}px)`);
+import Leesoyuun from "../../assets/img/Test.png";
 
 export const Whole = styled.div`
   display: flex;
   align-items: flex-start;
   justify-content: center;
   flex-direction: column;
-  width: max-content;
+  width: auto;
   margin-right: 10vw;
 `;
 export const Title = styled.div`
-  font-size: 44px;
+  font-size: 2.75rem;
   white-space: pre-wrap;
   line-height: 1.4;
   font-weight: 700;
+  ${media[2]} {
+    font-size: 1.75rem;
+  }
 `;
 export const SubDescription = styled.div`
   color: #aeaeae;
-  font-size: 19.5px;
+  font-size: 1.12rem;
   white-space: pre-wrap;
   line-height: 1.5;
   margin-top: 6px;
   margin-bottom: 4px;
+  ${media[2]} {
+    font-size: 1rem;
+  }
 `;
 export const MoreAbout = styled.div`
   cursor: pointer;
   font-size: 24px;
   &:hover {
     text-decoration: line-through;
+  }
+`;
+export const Container = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${media[2]} {
+    display: none;
+  }
+`;
+export const LiquidShape = styled.div`
+  border: 2px solid black;
+  width: 250px;
+  height: 250px;
+  background-image: url(${Leesoyuun});
+  background-size: cover;
+  animation: liquid 8s ease-in-out infinite;
+
+  border-radius: 60% 40% 30% 70% / 60% 30% 70% 40%;
+  @keyframes liquid {
+    0% {
+      border-radius: 60% 40% 30% 70% / 60% 30% 70% 40%;
+    }
+    50% {
+      border-radius: 30% 60% 70% 40% / 50% 60% 30% 60%;
+    }
+    100% {
+      border-radius: 60% 40% 30% 70% / 60% 30% 70% 40%;
+    }
+  }
+  ${media[2]} {
+    display: none;
   }
 `;
