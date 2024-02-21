b9baf0715469bbdb15bb9a1da4902560664efef3
leesoyuun <leesoyun4206@naver.com>
2024-02-21 23:45:23 +0900

    Add: 메뉴 클릭시 해당 페이지로 스크롤

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import * as h from "./HeaderStyle";
 import GitLogo from "../../assets/img/github.svg";
 import Velog from "../../assets/img/velog.svg";
 
-const Header = () => {
+const Header = ({ scrollToSection, homeRef, aboutRef, projectRef }) => {
+  const [subMenuOpen, setSubMenuOpen] = useState(false);
   return (
-    <h.total>
-      <h.myName>{`LEE SOYUN`}</h.myName>
-      <h.hamburgerMenu/>
-      <h.subMenuTotal>
-        <h.subMenu>{`HOME`}</h.subMenu>
-        <h.subMenu>{`ABOUT ME`}</h.subMenu>
-        <h.subMenu>{`PROJECT`}</h.subMenu>
-        <h.subMenu>{`TECH SKILL`}</h.subMenu>
-        <a href="https://github.com/leesoyuun" target="_blank">
-          <h.subImg src={GitLogo} />
-        </a>
-        <a href="https://velog.io/@leesoyuun/posts" target="_blank">
-          <h.subImg src={Velog} />
-        </a>
-      </h.subMenuTotal>
-    </h.total>
+    <>
+      {subMenuOpen && (
+        <h.subMenuTotal>
+          <h.close onClick={() => setSubMenuOpen(false)} />
+          <h.subMenus>
+            <h.subMenu>{`HOME`}</h.subMenu>
+            <h.subMenu>{`ABOUT ME`}</h.subMenu>
+            <h.subMenu>{`PROJECT`}</h.subMenu>
+            <h.subMenu>{`TECH SKILL`}</h.subMenu>
+          </h.subMenus>
+          <h.subBottom></h.subBottom>
+        </h.subMenuTotal>
+      )}
+      <h.total>
+        <h.myName onClick={() => scrollToSection(homeRef)}>{`LEE SOYUN`}</h.myName>
+        <h.hamburgerMenu onClick={() => setSubMenuOpen(true)} />
+        <h.MenuTotal>
+          <h.Menu onClick={() => scrollToSection(homeRef)}>{`HOME`}</h.Menu>
+          <h.Menu onClick={() => scrollToSection(aboutRef)}>{`ABOUT ME`}</h.Menu>
+          <h.Menu onClick={() => scrollToSection(projectRef)}>{`PROJECT`}</h.Menu>
+          <h.Menu>{`TECH SKILL`}</h.Menu>
+          <a href="https://github.com/leesoyuun" target="_blank">
+            <h.subImg src={GitLogo} />
+          </a>
+          <a href="https://velog.io/@leesoyuun/posts" target="_blank">
+            <h.subImg src={Velog} />
+          </a>
+        </h.MenuTotal>
+      </h.total>
+    </>
   );
 };
 export default Header;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-// import MainStyle from './MainStyle';
+import React, {useState, useRef} from "react";
 import * as c from "../../Component/Common/CommonStyle";
 import Header from "../../Component/Header/Header";
 import Project from "../../Component/Project/Project";
@@ -7,12 +6,35 @@ import Introduce from "../../Component/Introduce/Introduce";
 import AboutMe from "../../Component/AboutMe/AboutMe";
 
 const Main = () => {
+  const [currentPage, setCurrentPage] = useState('home');
+  const homeRef = useRef(null);
+  const aboutRef = useRef(null);
+  const projectRef = useRef(null);
+  const scrollToSection = (ref) => {
+    if (ref.current !== null) {
+      const offset = ref.current.offsetTop-120;
+      window.scrollTo({
+        top: offset,
+        behavior: 'smooth',
+      });
+    }
+  };
   return (
     <>
-      <Header />
+      <Header scrollToSection={scrollToSection}
+        homeRef={homeRef}
+        aboutRef={aboutRef}
+        projectRef={projectRef}/>
       <c.total>
-        <Introduce />
-        <AboutMe />
+        <div ref={homeRef}>
+          <Introduce />
+        </div>
+        <div ref={aboutRef}>
+          <AboutMe/>
+        </div>
+        <div ref={projectRef}>
+          <Project/>
+        </div>
       </c.total>
     </>
   );
