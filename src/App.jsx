9e0403fbb3e479b7a00f09a735df697b8a0e12e6
leesoyuun <leesoyun4206@naver.com>
2024-02-21 14:00:28 +0900

    Modify: 기초 설정 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Main from "./pages/Main/Main";
+import AboutMe from './Component/AboutMe/AboutMe';
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Main/>} />
+        <Route path="/aboutme" element={<AboutMe/>} />
       </Routes>
     </Router>
   );
