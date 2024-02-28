cc687fc9088489a5d836a45fb528eb3911b6b911
leesoyuun <leesoyun4206@naver.com>
2024-02-28 22:56:50 +0900

    Add: 공통스타일 요소 추가

--- src/Component/Common/CommonStyle.jsx
+++ src/Component/Common/CommonStyle.jsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 
 export const total = styled.div`
-  padding: 0px 3%;
+  padding: 0px 6%;
   min-height: 100vh;
   min-width: 100vw;
   box-sizing : border-box;
@@ -9,8 +9,21 @@ export const total = styled.div`
 `;
 export const Flex = styled.div`
   display: flex;
+  flex-wrap: wrap;
+`;
+export const FlexCenter = styled(Flex)`
+  align-items: center;
 `;
 export const FlexBetween = styled.div`
   display: flex;
   justify-content: space-between;
+  flex-wrap: wrap;
+`;
+export const FlexCol = styled.div`
+  display: flex;
+  flex-direction: columns;
+`;
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: max-content repeat(1, 1fr);
 `;
