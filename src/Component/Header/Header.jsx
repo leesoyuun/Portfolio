60ed13c332fa16351a0353d9e12cc38e242e6624
leesoyuun <leesoyun4206@naver.com>
2024-02-28 22:56:05 +0900

    Add: subheader에 이동되는 스크롤 추가

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -3,7 +3,7 @@ import * as h from "./HeaderStyle";
 import GitLogo from "../../assets/img/github.svg";
 import Velog from "../../assets/img/velog.svg";
 
-const Header = ({ scrollToSection, homeRef, aboutRef, projectRef }) => {
+const Header = ({ scrollToSection, homeRef, aboutRef, projectRef, techSkillRef }) => {
   const [subMenuOpen, setSubMenuOpen] = useState(false);
   return (
     <>
@@ -11,10 +11,10 @@ const Header = ({ scrollToSection, homeRef, aboutRef, projectRef }) => {
         <h.subMenuTotal>
           <h.close onClick={() => setSubMenuOpen(false)} />
           <h.subMenus>
-            <h.subMenu>{`HOME`}</h.subMenu>
-            <h.subMenu>{`ABOUT ME`}</h.subMenu>
-            <h.subMenu>{`PROJECT`}</h.subMenu>
-            <h.subMenu>{`TECH SKILL`}</h.subMenu>
+            <h.subMenu onClick={() => scrollToSection(homeRef,sub)}>{`HOME`}</h.subMenu>
+            <h.subMenu onClick={() => scrollToSection(aboutRef,sub)}>{`ABOUT ME`}</h.subMenu>
+            <h.subMenu onClick={() => scrollToSection(projectRef,sub)}>{`PROJECT`}</h.subMenu>
+            <h.subMenu onClick={() => scrollToSection(techSkillRef,sub)}>{`TECH SKILL`}</h.subMenu>
           </h.subMenus>
           <h.subBottom></h.subBottom>
         </h.subMenuTotal>
@@ -26,7 +26,7 @@ const Header = ({ scrollToSection, homeRef, aboutRef, projectRef }) => {
           <h.Menu onClick={() => scrollToSection(homeRef)}>{`HOME`}</h.Menu>
           <h.Menu onClick={() => scrollToSection(aboutRef)}>{`ABOUT ME`}</h.Menu>
           <h.Menu onClick={() => scrollToSection(projectRef)}>{`PROJECT`}</h.Menu>
-          <h.Menu>{`TECH SKILL`}</h.Menu>
+          <h.Menu onClick={() => scrollToSection(techSkillRef)}>{`TECH SKILL`}</h.Menu>
           <a href="https://github.com/leesoyuun" target="_blank">
             <h.subImg src={GitLogo} />
           </a>
